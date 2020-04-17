c501e9a097003ba584aa50bee6a69a4f0ffc1bb5
NikitaIT <NIKITA2008-101@yandex.ru>
2020-04-18 02:16:06 +0300

    auto_commit

--- git_push.bat
+++ git_push.bat
@@ -1,1 +1,8 @@
-bundle exec jekyll build && git add --all && git commit -m "auto_commit" && git push origin dev && cd _site && git add --all && git commit -m "auto_commit" && git push origin master
+git add --all
+git commit -m "auto_commit"
+git push origin dev
+cd _site
+git add --all
+git commit -m "auto_commit"
+git push origin master
+cd ../
