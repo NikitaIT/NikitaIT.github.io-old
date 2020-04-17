e014a7973d757435e7e6d4ed7b94499d6e6f3b80
NikitaIT <NIKITA2008-101@yandex.ru>
2020-04-18 01:40:48 +0300

    auto_commit

--- git_push.bat
+++ git_push.bat
@@ -1,7 +1,7 @@
-git add * 
+git add --all
 git commit -m "auto_commit" 
 git push origin master 
 cd _site 
-git add * 
+git add --all
 git commit -m "auto_commit" 
-git push origin master 
+git push origin master
